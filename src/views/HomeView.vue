bc33f7f1e5c9c4c544610dc958793a1970538b41
Luana <lulugomesfeliciano@gmail.com>
2023-08-29 19:41:56 -0300

    pokedex

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="container text-center">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 15rem;">
+              <img src="src/assets/pikachu.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <h1>container2</h1>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
